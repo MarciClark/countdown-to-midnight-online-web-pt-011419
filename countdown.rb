c0bdd8b5cbc7e8da6d13ad5a1b9d18f93f692d95
Marci Clark <marcig612@gmail.com>
2019-01-18 16:19:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,12 @@
+def countdown 
+end
+
 x = l
 while x < 10 
 puts "{x} seconds"
 x -= 10
 end
 
-def coundtown
-end
 
 it 'returns HAPPY NEW YEAR!' do
       expect(countdown(10)).to eq "HAPPY NEW YEAR!"
